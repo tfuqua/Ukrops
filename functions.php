150e434e66e8e4d6c453e435a039a8bf10774079
Taylor Fuqua <tfuqua@gwu.edu>
2017-03-16 14:02:02 -0400

    Updated styling for new store locator layout

--- functions.php
+++ functions.php
@@ -130,18 +130,6 @@ function change_wp_search_size($queryVars) {
 }
 add_filter('request', 'change_wp_search_size');
 
-
-add_action( 'pre_get_posts', 'my_change_sort_order');
-function my_change_sort_order($query){
-  if(is_archive()):
-   //If you wanted it for the archive of a custom post type use: is_post_type_archive( $post_type )
-     //Set the order ASC or DESC
-     $query->set( 'order', 'ASC' );
-     //Set the orderby
-     $query->set( 'orderby', 'menu_order' );
-  endif;
-};
-
 require_once('menu_walker.php');
 
 function excerpt($num) {
@@ -152,3 +140,17 @@ function my_gallery_style() {
     return "";
 }
 add_filter( 'use_default_gallery_style', '__return_false' );
+
+add_filter( 'template_include', 'portfolio_page_template', 99 );
+
+function portfolio_page_template( $template ) {
+
+	if ( is_page( 'portfolio' )  ) {
+		$new_template = locate_template( array( 'portfolio-page-template.php' ) );
+		if ( '' != $new_template ) {
+			return $new_template ;
+		}
+	}
+
+	return $template;
+}
---
+++ templates/store-locator.php
@@ -0,0 +1,9 @@
+<?php
+/* Template Name: Store Locator */
+
+
+$storeLocator = ABSPATH . 'wp-content/plugins/ukrops-store-locator/lib/render-store-locator.php';
+require_once($storeLocator);
+view_store_locator();
+
+?>
